1ed3c65c46929857ebe55603a8a3e15eb51e93a6
jsunthon <jsunthon@gmail.com>
2016-05-10 13:37:48 -0700

    restaurant details are now visible as textviews in the restaurantfragment

--- app/src/main/java/com/bignerdranch/android/randomrestaurants/RestaurantFragment.java
+++ app/src/main/java/com/bignerdranch/android/randomrestaurants/RestaurantFragment.java
@@ -1,6 +1,5 @@
 package com.bignerdranch.android.randomrestaurants;
 
-import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -30,11 +29,11 @@ public class RestaurantFragment extends Fragment {
 
         UUID restaurantId = (UUID) getActivity().getIntent().getSerializableExtra(RestaurantActivity.EXTRA_RESTAURANT_ID);
         restaurant = RestaurantLab.get(getActivity()).getRestaurant(restaurantId);
-//        if (intent != null && intent.hasExtra(EXTRA_RESTAURANT_ID, )) {
-//            String title = intent.getStringExtra(Intent.EXTRA_TEXT);
-//            ((TextView) rootView.findViewById(R.id.detail_text)).setText(title);
-//        }
-        ((TextView) rootView.findViewById(R.id.detail_text)).setText(restaurant.getName());
+
+        ((TextView) rootView.findViewById(R.id.restaurant_name_detail_text)).setText(restaurant.getName());
+        ((TextView) rootView.findViewById(R.id.restaurant_address_detail_text)).setText(restaurant.getAddress());
+        ((TextView) rootView.findViewById(R.id.restaurant_phone_detail_text)).setText(restaurant.getPhone());
+        ((TextView) rootView.findViewById(R.id.restaurant_rating_detail_text)).setText(String.valueOf(restaurant.getRating()));
 
         return rootView;
     }
