34851abe88f48f994782582116194e5af6319578
jsunthon <jsunthon@gmail.com>
2016-05-19 17:00:16 -0700

    added settings functionality for search radius and max # of results

--- app/src/main/java/com/bignerdranch/android/randomrestaurants/SettingsActivity.java
+++ app/src/main/java/com/bignerdranch/android/randomrestaurants/SettingsActivity.java
@@ -51,6 +51,8 @@ public class SettingsActivity extends PreferenceActivity
         // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
         // updated when the preference changes.
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_max_results_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_search_radius_key)));
     }
 
     /**
