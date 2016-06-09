22d4088d48ec8956f57f8b480657e042afef3100
jsunthon <jsunthon@gmail.com>
2016-06-08 18:59:19 -0700

    can now touch a image and a bigger version will show up in a dialog (popup window like strcuture)

---
+++ app/src/main/java/com/bignerdranch/android/randomrestaurants/ImageViewFragment.java
@@ -0,0 +1,50 @@
+package com.bignerdranch.android.randomrestaurants;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class ImageViewFragment extends DialogFragment {
+    private ImageView mFoodImgView;
+    private LinearLayout mLinearOuter;
+
+    private static final String ARG_RESTAURANT_TITLE = "res_title";
+    private static final String ARG_RESTAURANT_IMG_URL = "res_img_url";
+
+    public static ImageViewFragment newInstance(String restaurantName, String restaurantImgUrl) {
+        Bundle args = new Bundle();
+        args.putString(ARG_RESTAURANT_TITLE, restaurantName);
+        args.putString(ARG_RESTAURANT_IMG_URL, restaurantImgUrl);
+        ImageViewFragment imageViewFragment = new ImageViewFragment();
+        imageViewFragment.setArguments(args);
+        return imageViewFragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        String restaurantName = getArguments().getString(ARG_RESTAURANT_TITLE);
+        String imgUrl = getArguments().getString(ARG_RESTAURANT_IMG_URL);
+
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.view_food, null);
+
+        mFoodImgView = (ImageView) v.findViewById(R.id.food_img_view);
+        mFoodImgView.setAdjustViewBounds(true);
+        mFoodImgView.setScaleType(ImageView.ScaleType.FIT_XY);
+        new DownloadImageTask(mFoodImgView)
+                .execute(imgUrl);
+
+        mLinearOuter = (LinearLayout) v.findViewById(R.id.view_food_linear_layout);
+        mLinearOuter.removeAllViews();
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(mFoodImgView)
+                .setTitle(restaurantName)
+                .setPositiveButton(android.R.string.ok, null).create();
+    }
+}
